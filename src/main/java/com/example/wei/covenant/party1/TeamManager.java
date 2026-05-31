91d5e58cb62a2a22ca5a05eb7116201eedd0400c
konglong <2505664995@qq.com>
2026-05-31 16:09:45 +0800

    2

--- src/main/java/com/example/wei/covenant/party1/TeamManager.java
+++ src/main/java/com/example/wei/covenant/party1/TeamManager.java
@@ -3,18 +3,30 @@ package com.example.wei.covenant.party1;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 
+import javax.swing.*;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 public class TeamManager {
-    private Map<String, Team> teams;
-    private Map<UUID,String>playerTeamMap;
+    private final Map<UUID,TeamSlim> teams=new HashMap<>();
+    private final Map<UUID, UUID>playerTeamMap=new HashMap<>();
 
-    private MinecraftServer server;
-    public TeamManager(MinecraftServer server){this.server = server;}
-    public TeamSlim createTeam(String ServerPlayer){
+    private final MinecraftServer server;
+    public TeamManager(MinecraftServer server){
+        this.server = server;
+    }
 
+    public TeamSlim createTeam(UUID leaderUUID , String leaderName){
+        if(playerTeamMap.containsKey(leaderUUID)){return null;}
+        UUID teamUUID = UUID.randomUUID();
+        String teamName = leaderName+"'s Team";
+        TeamSlim newTeam = new TeamSlim(teamUUID,teamName,leaderUUID);
+
+        teams.put(teamUUID,newTeam);
+        playerTeamMap.put(leaderUUID,teamUUID);
+        return newTeam;
     }
     public void disbandTeam(String ServerPlayer){}
     public Optional<Team> getPlayerTeam(ServerPlayer player){}
